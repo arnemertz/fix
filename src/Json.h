89d26103b0ba3d7c8d2e70aef0ee62ee523975ef
Arne <arnemertz@github.com>
2016-07-19 22:28:12 +0200

    start using nlohmann::json
    Use typedef Fix::Json throughout the project

---
+++ src/Json.h
@@ -0,0 +1,9 @@
+#ifndef FIX_JSON_H
+#define FIX_JSON_H
+
+#include "json.hpp"
+namespace Fix {
+  using Json = nlohmann::json;
+}
+
+#endif //FIX_JSON_H
--- src/Server/RESTRequestHandler.cpp
+++ src/Server/RESTRequestHandler.cpp
@@ -2,13 +2,21 @@
 #include "Poco/Net/HTTPServerResponse.h"
 #include "Poco/Net/HTTPServerRequest.h"
 
+#include "Json.h"
+
 using namespace Fix::Server;
 
 void RESTRequestHandler::handleRequest(Poco::Net::HTTPServerRequest& request, Poco::Net::HTTPServerResponse& response)
 {
+
   response.setChunkedTransferEncoding(true);
   response.setContentType("application/json");
 
   std::ostream& ostr = response.send();
-  ostr << R"({ "location": ")" + request.getURI() + R"(" })";
+  ostr << Json {
+    { "error", {
+      { "message", "Unsupported operation." }
+    }},
+    { "links",  request.getURI() }
+  };
 }
