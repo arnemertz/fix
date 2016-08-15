ff34c8be1e368fbfb070014a5254c1a50cf18b4e
Arne <arnemertz@github.com>
2016-08-15 09:00:14 +0200

    refactor step definitions

--- test/behave/steps/issue_steps.py
+++ test/behave/steps/issue_steps.py
@@ -11,13 +11,10 @@ def step_impl(context):
 @when('we create issues')
 def step_impl(context):
     table = context.table
+    issue_keys = table.headings
     for row in table:
-        issue_data = {}
-        for key in table.headings:
-            issue_data[key] = row[key]
-        issue = {'data': issue_data}
-        r = requests.post('http://localhost:8080/issue/new', json=issue)
-        assert r.status_code == 200
+        issue_json = extract_issue_json(issue_keys, row)
+        create_issue(issue_json)
 
 
 @then('an issue file "{file_name}" exists in the repository')
@@ -25,4 +22,17 @@ def step_impl(context, file_name):
     issue_dir = context.tempdir + '/.fix/issues/'
     expected_file = Path(issue_dir + file_name)
     assert expected_file.exists()
+
+
+def extract_issue_json(issue_keys, row):
+    issue_data = {}
+    for key in issue_keys:
+        issue_data[key] = row[key]
+    issue_json = {'data': issue_data}
+    return issue_json
+
+
+def create_issue(issue_json):
+    r = requests.post('http://localhost:8080/issue/new', json=issue_json)
+    assert r.status_code == 200
 
