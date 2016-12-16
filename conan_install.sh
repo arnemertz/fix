62d987b79814df26258f140c52efce51f2a02502
Arne Mertz <github@arne-mertz.de>
2016-12-16 07:49:18 +0100

    adapt Conan workflow to new possibilities in CLion 2016.3.2 EAP

--- conan_install.sh
+++ conan_install.sh
@@ -1,10 +1,17 @@
 #! /bin/bash
 
-mkdir -p build
-cd build
-CONAN_ARGS="-s build_type=$1"
-if [ $1 == Debug ]; then
-    CONAN_ARGS+=" -o Poco:cxx_14=True"
+CONFIGURATION=$1
+
+if [ "${CONFIGURATION}" == "Debug" ]; then
+    BUILD_DIR=cmake-build-debug
+elif [ "$CONFIGURATION" == "Release" ]; then
+    BUILD_DIR=cmake-build-release
+else
+    echo "Configuration missing (Debug|Release)"
+    exit 1
 fi
-CONAN_ARGS+=" --build missing"
+mkdir -p ${BUILD_DIR}
+cd ${BUILD_DIR}
+
+CONAN_ARGS="-s build_type=${CONFIGURATION} --build missing"
 conan install .. ${CONAN_ARGS}
