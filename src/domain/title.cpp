1cd5ea38e7330eb0e5909601f4e9c93f913279da
Arne Mertz <arnemertz@users.noreply.github.com>
2021-08-28 13:05:55 +0200

    refactor title::create
    function has grown too big

--- src/domain/title.cpp
+++ src/domain/title.cpp
@@ -10,21 +10,47 @@ using namespace fix::domain;
 constexpr size_t MIN_LENGTH = 6;
 constexpr size_t MAX_LENGTH = 120;
 
+namespace {
+expected<std::string_view> check_trimmed(std::string_view text);
+expected<std::string_view> check_length(std::string_view text);
+expected<std::string_view> check_charset(std::string_view text);
+} // namespace
+
 title::title(std::string_view text) : text{text} {}
 
 expected<title> title::create(std::string_view text) {
+  // clang-format off
+  return check_trimmed(text)
+      .and_then(check_length)
+      .and_then(check_charset)
+      .map([](auto text){ return title{text}; });
+  // clang-format on
+}
+
+std::string title::to_string() const {
+  return text;
+}
+
+namespace {
+expected<std::string_view> check_trimmed(std::string_view text) {
   const auto trimmed_length = ranges::size(text | ranges::views::trim([](char c) { return isspace(c); }));
   if (trimmed_length != text.length()) {
     return unexpected(domain_error::TITLE_NOT_TRIMMED);
   }
+  return text;
+}
 
+expected<std::string_view> check_length(std::string_view text) {
   if (text.length() < MIN_LENGTH) {
     return unexpected(domain_error::TITLE_TOO_SHORT);
   }
   if (text.length() > MAX_LENGTH) {
     return unexpected(domain_error::TITLE_TOO_LONG);
   }
+  return text;
+}
 
+expected<std::string_view> check_charset(std::string_view text) {
   if (ranges::any_of(text, [](int c) { return (c > CHAR_MAX) || (c < 0); })) {
     return unexpected(domain_error::TITLE_HAS_NON_ASCII_CHARS);
   }
@@ -34,10 +60,7 @@ expected<title> title::create(std::string_view text) {
   if (text.find_first_of("\\`") != std::string_view::npos) {
     return unexpected(domain_error::TITLE_HAS_SPECIAL_PUNCTUATION);
   }
-
-  return title{text};
-}
-
-std::string title::to_string() const {
   return text;
 }
+
+} // namespace
